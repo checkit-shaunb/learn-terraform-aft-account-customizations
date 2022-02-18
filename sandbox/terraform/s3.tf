2c2ac4ffbd81b7e69199d454206d086bda9900db
Shaun Bradley <shaun.bradley@checkit.net>
2022-02-18 13:34:10 +0000

    testing custom account customizations

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -3,7 +3,3 @@ data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
 }
-
-resource "aws_s3_bucket" "sandbox_bucket2" {
-  bucket = "aft-sandbox-2-${data.aws_caller_identity.current.account_id}"
-}
